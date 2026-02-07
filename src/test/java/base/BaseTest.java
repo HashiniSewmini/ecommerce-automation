394c417c2879953cb735737e54510cfd0f4ca1d3
HashiniSewmini <hashinisewmini01@gmail.com>
2026-02-07 10:22:48 +0530

    Add base test infrastructure with WebDriver setup and teardown

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,34 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://automationexercise.com/");
+    }
+
+    @BeforeMethod
+    public void goToHome() {
+        driver.get("https://automationexercise.com/");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+}
